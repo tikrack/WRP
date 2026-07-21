6451169f22af8e0dd2a3fab320fe4d6f81035e12
Tikrack <tikrackcode@gmail.com>
2026-07-21 10:25:19 +0330

    adding login and register user controll

---
+++ Application/Pages/Authentication/LoginPage.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Application.Pages.Authentication
+{
+    partial class LoginPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginPage));
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(155, 50);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(90, 90);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(64, 209);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(272, 32);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(263, 176);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 27);
+            label1.TabIndex = 2;
+            label1.Text = "نام کاربری";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(64, 292);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(272, 32);
+            textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(275, 262);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 27);
+            label2.TabIndex = 2;
+            label2.Text = "رمز عبور";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(41, 44, 44);
+            button1.Cursor = Cursors.Hand;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(64, 364);
+            button1.Name = "button1";
+            button1.Size = new Size(272, 48);
+            button1.TabIndex = 2;
+            button1.Text = "ورود";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Cursor = Cursors.Hand;
+            label3.Font = new Font("Vazirmatn", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(41, 44, 44);
+            label3.Location = new Point(166, 432);
+            label3.Name = "label3";
+            label3.RightToLeft = RightToLeft.Yes;
+            label3.Size = new Size(170, 26);
+            label3.TabIndex = 3;
+            label3.Text = "اکانتی ندارید؟ ثبت نام کنید.";
+            label3.Click += label3_Click;
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(9F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Vazirmatn", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "LoginPage";
+            Size = new Size(400, 480);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+    }
+}
---
+++ Application/Pages/Authentication/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Application.Pages.Authentication
+{
+    public partial class LoginPage : UserControl
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            Form? MainForm = NavigationService.MainForm;
+
+            MainForm?.ClientSize = new Size(this.Size.Width, this.Size.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new RegisterPage());
+        }
+    }
+}
