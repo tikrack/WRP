b41254b6b71bb6834afdc2fafba594eab139084e
Tikrack <tikrackcode@gmail.com>
2026-07-21 10:26:47 +0330

    adding application

---
+++ Application/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using Application.Pages.Authentication;
+using Application.Services;
+using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Text;
+    using System.Windows.Forms;
+
+namespace Application
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            NavigationService.Panel = panel1;
+            NavigationService.MainForm = this;
+
+            NavigationService.Navigate(page: new LoginPage());
+        }
+    }
+}
---
+++ Application/Services/NavigationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services
+{
+    internal class NavigationService
+    {
+        public static Form? MainForm { get; set; }
+        public static Panel? Panel { get; set; }
+
+        public static void Navigate(UserControl page)
+        {
+            if (Panel == null) return;
+
+            Panel.Controls.Clear();
+
+            page.Dock = DockStyle.Fill;
+
+            Panel.Controls.Add(page);
+        }
+    }
+}
