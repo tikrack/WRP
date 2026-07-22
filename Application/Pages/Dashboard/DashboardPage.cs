1f05e5fdc7352867177286a55d9a996f45ad1b37
Tikrack <tikrackcode@gmail.com>
2026-07-22 07:43:23 +0330

    adding dashboard page

---
+++ Application/Pages/Dashboard/DashboardPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Application.Pages.Dashboard
+{
+    public partial class DashboardPage : UserControl
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
