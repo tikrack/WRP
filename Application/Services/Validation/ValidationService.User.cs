4dac95fbc280a664e5f91b2dbb38d8073de16cd3
Tikrack <tikrackcode@gmail.com>
2026-07-22 11:47:01 +0330

    add user validation

--- Application/Services/Validation/ValidationService.User.cs
+++ Application/Services/Validation/ValidationService.User.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Entities;
+using Application.Services.Storage;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,11 +10,26 @@ namespace Application.Services.Validation
     {
         public static List<string> Validation(User user)
         {
+            StorageService storageService = new StorageService();
+
             List<string> errors = new List<string>();
 
-            if(string.IsNullOrEmpty(user.UserName))
+            if (string.IsNullOrEmpty(user.FirstName))
             {
-                errors.Add("");
+                errors.Add("فیلد نام اجباری است.");
+            }
+
+            if (string.IsNullOrEmpty(user.UserName))
+            {
+                errors.Add("نام کاربری اجباری است.");
+            }else if(storageService?.Where(us => us.UserName == user.UserName).Count > 0)
+            {
+                errors.Add("نام کاربری قبلا انتخاب شده است.");
+            }
+
+            if (string.IsNullOrEmpty(user.Password) || user.Password.Length < 3)
+            {
+                errors.Add("رمز عبور باید حداقل ۳ کاراکتر باشد.");
             }
 
             return errors;
